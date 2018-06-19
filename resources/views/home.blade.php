2a52056cef1e18ddb04b4596857bf8d8db6d2ccc
Daniel Berry <dberry@carnegietechnologies.com>
2018-06-19 09:15:30 -0500

    updates css and templates
    * Added css for more material buttonons
    * Changed the titles to use app.name
    * Adds card component
    * updates the home view to card component.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-12">
+            @component('components.card')
+                @slot('title')
+                    Dashboard
+                @endslot
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                    You are logged in!
-                </div>
-            </div>
+                You are logged in!
+            @endcomponent
         </div>
     </div>
 </div>
